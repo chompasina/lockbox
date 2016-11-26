5d34f4f2d078023a9a2144ae9690b75aaade283e
Tommasina <tommasina1@gmail.com>
2016-11-26 18:26:01 -0500

    Adds user can sign out spec

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     email "tom@gmail.com"
     username "tommasina"
     password "password"
+    password_confirmation "password"
   end
   # <factories for each model go here>
 end
