2ce61d073bd96fdbf1f4d441a8cd4f98e8d13b01
Tommasina <tommasina1@gmail.com>
2016-11-26 10:06:58 -0500

    Adds user can sign up spec- not passing yet

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "user can sign up" do
+  scenario "user is added when they sign up" do
+    visit root_path
+    expect(current_path).to eq(login_path)
+    
+    click_button "Sign Up"
+    expect(current_path).to eq(new_user_path)
+    expect(page).to_not have_content("Welcome, Tommasina")
+    expect(page).to_not have_content("Logout")
+    
+    fill_in "Username", with: "Tommasina"
+    fill_in "Password", with: "password"
+    fill_in "Email", with: "example@gmail.com"
+    fill_in "Password Confirmation", with: "password"
+    click_on "Create Account"
+    
+    expect(current_path).to eq(links_path)
+    expect(page).to have_content("Welcome, Tommasina")
+    expect(page).to have_button("Logout")
+  end
+end
