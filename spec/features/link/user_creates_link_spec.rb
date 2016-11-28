a3a53d51f49aafdd301c69a65c2eab7edd723654
Tommasina <tommasina1@gmail.com>
2016-11-28 10:19:37 -0500

    Adds validate URL functionality, test and sad path test passing

--- spec/features/link/user_creates_link_spec.rb
+++ spec/features/link/user_creates_link_spec.rb
@@ -16,5 +16,22 @@ RSpec.feature "user can create link" do
     
     expect(page).to have_content("New York Times")
     expect(page).to have_content("http://www.nytimes.com")
+    expect(Link.count).to eq(1)
+  end
+  
+  scenario "user gives a broken link" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit root_path
+    
+    fill_in "Title", with: "New York Times"
+    fill_in "URL", with: "broken.com"
+    click_button "Add link"
+    
+    expect(page).to have_content("Url is not a valid URL")
+    expect(page).to_not have_content("New York Times")
+    expect(page).to_not have_content("broken.com")
+    expect(Link.count).to eq(0)
   end
 end
