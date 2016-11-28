71796d7c839e3535351ede875bb2431d67f7ab98
Tommasina <tommasina1@gmail.com>
2016-11-28 10:20:02 -0500

    Adds stub to user creates link spec

--- spec/features/link/user_creates_link_spec.rb
+++ spec/features/link/user_creates_link_spec.rb
@@ -4,11 +4,8 @@ RSpec.feature "user can create link" do
   scenario "user is on links index page" do
     user = create(:user)
     
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit root_path
-    fill_in "Username", with: "tommasina"
-    fill_in "Password", with: "password"
-    click_button "Login"
     
     fill_in "Title", with: "New York Times"
     fill_in "URL", with: "http://www.nytimes.com"
