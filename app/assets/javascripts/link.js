e2c7d66ed8c10ce18d4ba0796050b57a9db198b4
Tommasina <tommasina1@gmail.com>
2016-11-28 14:03:47 -0500

    Adds update status with AJAX functionality, spec passing

--- app/assets/javascripts/link.js
+++ app/assets/javascripts/link.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 
 function changeStatus(){
   $('.links-list').on('click', '#toggle-link-status', function(e){
+    debugger;
     console.log("hit")
     var button = e.target;
     toggleStatus(button);
-    
   });
 }
 
@@ -18,5 +18,30 @@ function toggleStatus(button){
 }
 
 function storeNewStatus(button, newStatus){
-    var linkId = $(button).data('id')
+  var linkId = $(button).data('id')
+    $.ajax({
+      method: "PUT",
+      url: "/api/v1/links/" + linkId + '.json',
+      data: {id: linkId, read: newStatus},
+      success: function(){
+        var id = $(button).data('id');
+        var row = $('.link-row[data-id="' + id +'"]');
+        updateButton(id, row);
+      }
+    })
+}
+
+function updateButton(id, row){
+  var $button = $('.button[data-id="' + id + '"]')
+  var $url = $('.url-field[data-id="' + id + '"]')
+  var $linkRead = $('#link-status-'+ id + '' )
+  if($button.text() === 'Mark as Unread'){
+      $button.text("Mark as Read");
+      $linkRead.text("Read? false");
+      $url[0].style['cssText'] = 'text-decoration: none;'
+  } else {
+    $button.text("Mark as Unread");
+    $url[0].style['cssText'] = 'text-decoration: line-through;';
+    $linkRead.text("Read? true")
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+  
+  namespace :api do
+    namespace :v1 do
+      resources :links
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
