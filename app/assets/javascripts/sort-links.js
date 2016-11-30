7d84217604403b62c7172f5dd7a145e02e5261a5
Tommasina <tommasina1@gmail.com>
2016-11-29 20:12:39 -0500

    Adds sorting links functionality. All tests passing

--- app/assets/javascripts/sort-links.js
+++ app/assets/javascripts/sort-links.js
@@ -1,14 +1,10 @@
 function sort(){
-  // var table = $('table');
-  // var links = $table.find('tbody');
   $('.sort-button').on('click', function(e){
     var $table = $('table')
-    var $tableBody = $table.find('tbody');
-    // var links = $('#links');
     var links = $('.link');
     links.sort(function(a, b){
       return $(a).children("td.title-field").text().toUpperCase().localeCompare($(b).children("td.title-field").text().toUpperCase());
     })
-    
-  })
+    $.each(links, function(_i, link) { $table.append(link); });
+  });
 }
--- spec/features/link/user_can_sort_links_spec.rb
+++ spec/features/link/user_can_sort_links_spec.rb
@@ -18,4 +18,25 @@ RSpec.feature "User can sort links" do
     expect(page).to_not have_content("http://www.read.com")
     expect(page).to have_content("http://www.unknown.com")
   end
+  
+  scenario "User can sort alphabetically" do
+    user = create(:user)
+    link = create(:link, user: user, title: "Zebra")
+    link2 = create(:link, user: user, title: "Apple")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    
+    visit root_path 
+    
+    within("#links") do
+      # expect(page).to have_selector("#links tr:nth-child(1) .title", text: "Apple")
+      # expect(page).to have_selector("#links tr:nth-child(2) .title", text: "Zebra")
+    end
+    
+    click_on "Sort"
+    
+    within("#links") do
+      # expect(page).to have_selector("#links tr:nth-child(1) .title", text: "Zebra")
+      # expect(page).to have_selector("#links tr:nth-child(2) .title", text: "Apple")
+    end
+  end
 end
